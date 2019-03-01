85e1ce26a90dd9fe2a27aced11ae45164403d0ee
Anand Vijay Rajsri <avrajsri@gmail.com>
2019-03-01 21:51:21 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 from tkinter import *
 
-
 root=Tk()
 root.title("Fantasy Cricket")
 root.geometry("680x420")
 root.resizable(width=FALSE,height=FALSE)
+root.configure(background='#FFFFFF')
 # -----------Show Root win in center--------------
 def center(win):
     win.update_idletasks()
@@ -36,54 +36,57 @@ def EVALUATE():
 #0
 center(root)
 #1
-mb= Menubutton ( root, text="Manage Teams", relief=RAISED )
-mb.grid()
+mb= Menubutton ( root, text="Manage Teams",font=('Comic Sans MS',9), relief=RAISED )
+mb.place(x=0,y=0)
 mb.menu =  Menu ( mb, tearoff = 0 )
 mb["menu"] =  mb.menu
 
 mb.config(background="#CCCCCC")
 mb.menu.config(background="#CCCCCC")
 
-mb.menu.add_command(label="NEW Team", command=NEW)
-mb.menu.add_command(label="OPEN Team", command=OPEN)
-mb.menu.add_command(label="SAVE Team", command=SAVE)
-mb.menu.add_command(label="EVALUATE Team", command=EVALUATE)
+mb.menu.add_command(label="NEW Team", command=NEW,font=('Comic Sans MS',9))
+mb.menu.add_command(label="OPEN Team", command=OPEN,font=('Comic Sans MS',9))
+mb.menu.add_command(label="SAVE Team", command=SAVE,font=('Comic Sans MS',9))
+mb.menu.add_command(label="EVALUATE Team", command=EVALUATE,font=('Comic Sans MS',9))
 mb.place(x=0,y=0)
 
-labelframe = LabelFrame(root, text="Your Selection")
-labelframe.place(x=45,y=45)
+fm = Frame(root, borderwidth=2, relief="groove")
+fm.place(x=45,y=45)
 
-left = Label(labelframe, text="")
-left.pack()
-left = Label(labelframe, text="     Batsman(BAT)               "
-                              "     Bowers(BOW)                 "
-                              "     Allroundders(AR)             "
-                              "     Wicket-Keeper(WK)                 ")
-left.pack()
-left = Label(labelframe, text="")
-left.pack()
+Label(fm, text="Your Selection",font=('Comic Sans MS',8)).place(x=1,y=0)
 
-sbox1 = Text(labelframe, height=1, width=2, bg="white")
-sbox1.place(x=97, y=23)
-sbox1.tag_configure('color', foreground='#399B9B', font=('Calibri', 12, 'bold'))
-sbox1.insert(END, "##", 'color')
-sbox1.configure(state='disabled')
-sbox2 = Text(labelframe, height=1, width=2, bg="white")
-sbox2.place(x=230, y=23)
-sbox2.tag_configure('color', foreground='#399B9B', font=('Calibri', 12, 'bold'))
-sbox2.insert(END, "##", 'color')
-sbox2.configure(state='disabled')
-sbox3 = Text(labelframe, height=1, width=2, bg="white")
-sbox3.place(x=386, y=23)
-sbox3.tag_configure('color', foreground='#399B9B', font=('Calibri', 12, 'bold'))
-sbox3.insert(END, "##", 'color')
-sbox3.configure(state='disabled')
-sbox4 = Text(labelframe, height=1, width=2, bg="white")
-sbox4.place(x=544, y=23)
-sbox4.tag_configure('color', foreground='#399B9B', font=('Calibri', 12, 'bold'))
-sbox4.insert(END, "##", 'color')
-sbox4.configure(state='disabled')
+Label(fm, text="").pack()
+Label(fm, text="     Batsman(BAT)               "
+               "     Bowers(BOW)                 "
+               "     Allroundders(AR  )             "
+               "     Wicket-Keeper(WK)             ").pack()
+Label(fm, text="").pack()
 
+l1=Label(fm,text="Batsman (BAT)",font=('Comic Sans MS',9,"bold"))
+l2=Label(fm,text="Bowers (BOW)",font=('Comic Sans MS',9,"bold"))
+l3=Label(fm,text="Allroundders (AR)",font=('Comic Sans MS',9,"bold"))
+l4=Label(fm,text="Wicket-Keeper (WK)",font=('Comic Sans MS',9,"bold"))
+l1.place(x=3,y=23)
+l2.place(x=140,y=23)
+l3.place(x=275,y=23)
+l4.place(x=420,y=23)
 
+z=StringVar()
+z.set("##")
+#z.initialize("##")
+
+e1=Entry(fm,font=('arial',10,"italic"),bg="#F0F0F0",bd=0,fg='#399B9B',textvariable=z)
+e1.place(x=97, y=26,width=15)
+e2=Entry(fm,font=('arial',10,"italic"),bg="#F0F0F0",bd=0,fg='#399B9B',textvariable=z)
+e2.place(x=230, y=26,width=15)
+e3=Entry(fm,font=('arial',10,"italic"),bg="#F0F0F0",bd=0,fg='#399B9B',textvariable=z)
+e3.place(x=386, y=26,width=15)
+e4=Entry(fm,font=('arial',10,"italic"),bg="#F0F0F0",bd=0,fg='#399B9B',textvariable=z)
+e4.place(x=544, y=26,width=15)
+
+#e1.configure(state='disabled')
+#e2.configure(state='disabled')
+#e3.configure(state='disabled')
+#e4.configure(state='disabled')
 
 root.mainloop()
